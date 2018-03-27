29de9d93a6a6fe7a241c4ca77055825be3402a02
Yuichiro Iwai <yuichiro.iwai@gmail.com>
2018-03-28 08:49:43 +0900

    SpeedBasedBattle追加

--- scene/src/main/scala/com/yuiwai/moiwa/scene/SpeedBasedPvEBattle.scala
+++ scene/src/main/scala/com/yuiwai/moiwa/scene/SpeedBasedPvEBattle.scala
@@ -1,7 +1,10 @@
 package com.yuiwai.moiwa.scene
 
+import com.yuiwai.moiwa.battle.{EnemySide, PlayerSide, SideLike, SpeedBasedBattleLike}
 import com.yuiwai.moiwa.components.PvEBattleComponents
 
 case class SpeedBasedPvEBattle () extends PvEBattleComponents with SpeedBasedBattleLike {
-
+  override val playerSide: PlayerSide = _
+  override val enemySide: EnemySide = _
+  override val currentSide: SideLike = _
 }
